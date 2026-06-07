0a4aec2715d6d8c70ee76cf118f0eb8c5686a388
Jose Marquez <jose.marquez012@mymdc.net>
2026-06-07 18:24:50 -0400

    Initial Commit

---
+++ MemeCreator/MemeCreatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemeCreatorApp.swift
+//  MemeCreator
+//
+//  Created by user on 6/7/26.
+//
+
+import SwiftUI
+
+@main
+struct MemeCreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
